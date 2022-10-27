a31288754cf671020614db5c2712f75752e9bdaf
kritika dubey <kritika.dubey@cumminscollege.in>
2022-10-27 18:44:19 +0530

    a4/add

--- site/assignment_4/script.js
+++ site/assignment_4/script.js
@@ -1,5 +1,6 @@
 (function() {
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    console.log("the output is-");
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
@@ -10,4 +11,3 @@
     }
 })();
 
-})();
